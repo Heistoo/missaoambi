93ade968bf1fa5644eb82e634a7e1f2750578786
Heistoo <46348338+Heistoo@users.noreply.github.com>
2023-10-23 11:13:20 -0300

    apply js code

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,29 +1,29 @@
 
 // Script java to show and hide menu-layout & fechar-menu
-// const menuInterativoBtn = document.querySelector(".menu-layout");
-// const fecharMenuBtn = document.querySelector(".close-menu");
-// const nav = document.querySelector("nav");
-// const menuOverlay = document.querySelector(".menu-overlay");
+const menuInterativoBtn = document.querySelector(".menu-layout");
+const fecharMenuBtn = document.querySelector(".close-menu");
+const nav = document.querySelector("nav");
+const menuOverlay = document.querySelector(".menu-overlay");
 
-// function showMenu() {
-//     nav.classList.add("show");
-//     menuOverlay.classList.add("show");
-// }
+function showMenu() {
+    nav.classList.add("show");
+    menuOverlay.classList.add("show");
+}
 
-// function hideMenu() {
-//     nav.classList.remove("show");
-//     menuOverlay.classList.remove("show");
-// }
+function hideMenu() {
+    nav.classList.remove("show");
+    menuOverlay.classList.remove("show");
+}
 
-// menuInterativoBtn.addEventListener("click", () => {
-//     showMenu();
-// });
+menuInterativoBtn.addEventListener("click", () => {
+    showMenu();
+});
 
-// fecharMenuBtn.addEventListener("click", () => {
-//     hideMenu();
-// });
+fecharMenuBtn.addEventListener("click", () => {
+    hideMenu();
+});
 
-// //possivelmente fecha o overlay do menu (a barra cinza)
-// menuOverlay.addEventListener("click", () => {
-//     hideMenu();
-// });
+//possivelmente fecha o overlay do menu (a barra cinza)
+menuOverlay.addEventListener("click", () => {
+    hideMenu();
+});
