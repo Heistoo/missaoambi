a9e78f92d92aad7fa772e8f7fcdf7895245492f7
Heistoo <46348338+Heistoo@users.noreply.github.com>
2023-10-23 10:59:21 -0300

    create js for Button

---
+++ JS/scripts.js
@@ -0,0 +1,29 @@
+
+// Script java to show and hide menu-layout & fechar-menu
+// const menuInterativoBtn = document.querySelector(".menu-layout");
+// const fecharMenuBtn = document.querySelector(".close-menu");
+// const nav = document.querySelector("nav");
+// const menuOverlay = document.querySelector(".menu-overlay");
+
+// function showMenu() {
+//     nav.classList.add("show");
+//     menuOverlay.classList.add("show");
+// }
+
+// function hideMenu() {
+//     nav.classList.remove("show");
+//     menuOverlay.classList.remove("show");
+// }
+
+// menuInterativoBtn.addEventListener("click", () => {
+//     showMenu();
+// });
+
+// fecharMenuBtn.addEventListener("click", () => {
+//     hideMenu();
+// });
+
+// //possivelmente fecha o overlay do menu (a barra cinza)
+// menuOverlay.addEventListener("click", () => {
+//     hideMenu();
+// });
