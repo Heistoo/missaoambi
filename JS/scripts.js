2c81a3a6091cd9ef2583aa3631caddfd5484db3f
AllanKenzo <allan.takose@gmail.com>
2023-10-26 12:06:22 -0300

    carrossel aprimorado

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,6 +4,8 @@ const menuInterativoBtn = document.querySelector(".menu-layout");
 const fecharMenuBtn = document.querySelector(".fechar-menu");
 const nav = document.querySelector("nav");
 const menuOverlay = document.querySelector(".menu-overlay");
+const carrosselItens = document.querySelectorAll('.carrossel-item');
+let indiceAtual = 0;
 
 function showMenu() {
     nav.classList.add("show");
@@ -27,3 +29,28 @@ fecharMenuBtn.addEventListener("click", () => {
 menuOverlay.addEventListener("click", () => {
     hideMenu();
 });
+
+
+//funções do carrossel
+function mostrarSlide(index) {
+  carrosselItens.forEach(item => {
+    item.style.display = 'none';
+  });
+
+  carrosselItens[index].style.display = 'block';
+}
+
+function proximoSlide() {
+  indiceAtual = (indiceAtual + 1) % carrosselItens.length;
+  mostrarSlide(indiceAtual);
+}
+
+function anteriorSlide() {
+  indiceAtual = (indiceAtual - 1 + carrosselItens.length) % carrosselItens.length;
+  mostrarSlide(indiceAtual);
+}
+
+mostrarSlide(indiceAtual);
+
+document.getElementById('proxBtn').addEventListener('click', proximoSlide);
+document.getElementById('anterBtn').addEventListener('click', anteriorSlide);
